7d0bb8142900deffe1221f39564709eb964d9ba8
salmanbarani <salman.barani@postpay.io>
2023-02-08 15:27:12 +0300

    defninig db

---
+++ orm.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, MetaData, Table, ForeignKey, Integer, String
+from sqlalchemy.orm import mapper, relationship
+import model
+
+metadata = MetaData()
+
+order_lines = Table(
+    'order_lines', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('orderid', String(255)),
+)
+
+
+def start_mappers():
+    lines_mapper = mapper(model.OrderLine, order_lines)
