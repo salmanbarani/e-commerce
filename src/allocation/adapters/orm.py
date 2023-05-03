d1a432fb580deeca24a1c6bbbda2f934db936ce5
salmanbarani <salman.barani@postpay.io>
2023-05-03 13:33:07 +0300

    add remaning code

--- src/allocation/adapters/orm.py
+++ src/allocation/adapters/orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey
+from sqlalchemy import Table, MetaData, Column, Integer, String, Date, ForeignKey, event
 from sqlalchemy.orm import registry, relationship
 
 from allocation.domain import model
@@ -61,3 +61,8 @@ def start_mappers():
         products,
         properties={"batches": relationship(batches_mapper)}
     )
+
+
+@event.listens_for(model.Product, "load")
+def receive_load(product, _):
+    product.events = []
